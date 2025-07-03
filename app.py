456f28022df576f3e7b13d4e132131472f06c009
Anton Ivanov <sibiry-k@yandex.ru>
2025-07-04 00:16:02 +0700

    Add pypugjs and stylus

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.pug')
 
 
 if __name__ == '__main__':
